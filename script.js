1fdfc1e317bbc66c2168d0a71bd1f87cd8330a4f
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-04-19 17:52:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+function findMajorityElement(nums){
+	let count=0;
+	let candidate=null;
+	for(let num of nums){
+		if(count===0){
+			candidate=num;
+		}
+		count+=(num===candidate)?1:-1;
+	}
+	count=0;
+	for(let num of nums){
+		if(num === candidate){
+			count++;
+		}
+	}
+if(count > Math.floor(nums.length/2)){
+	return candidate;
+}
+else{
+	return -1;
+}
+}
